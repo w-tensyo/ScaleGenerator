52afb39122a21c629768bf7d3cb52303823cafc8
w.tensyo <dr.tensyo@gmail.com>
2019-09-03 00:09:39 +0900

    ラジオボタンでb表示、#表示の選択を可能に。 機能を有効にする方法がまだわかっていない。
    RadioButtonのidを取得してswitch文で判定↓
    string-arrayをGet取得してspinnerにsetすればいいんだと思っているが・・・。
    うまくいかず・・・。

--- app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
+++ app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.wtensyo.scalegenerator;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner leftSpinner;
     Spinner rightSpinner;
-
+    String[] selectTone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
