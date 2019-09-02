4fd828c58e723816ce44f86c829a7c9c7f98d177
w.tensyo <dr.tensyo@gmail.com>
2019-09-02 23:05:43 +0900

    string-arrayのプルダウンをspinnerで表示することに成功！

--- app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
+++ app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.wtensyo.scalegenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner leftSpinner;
+    Spinner rightSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
