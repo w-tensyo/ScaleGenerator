d57c3f0890b58d8327e62ad69f1d993d7372dd45
w.tensyo <dr.tensyo@gmail.com>
2019-09-11 00:31:56 +0900

    spinnerで選択したプルダウンをもとに、subStringで必要なだけの文字列にする処理まではできている・・・と思う。 が、疑問点がいくつか。 １：getSelectedItemPosition();の処理をここですることが正しいのかがわかっていない ２：return;の扱い方。 retrun result;と書くとエラーになる。なぜ？

--- app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
+++ app/src/main/java/com/example/wtensyo/scalegenerator/MainActivity.java
@@ -4,10 +4,12 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         //データ型変数leftSpinnerにleft_spinnerを入れる
         Spinner leftSpinner = (Spinner) findViewById(R.id.left_spinner);
 
@@ -31,6 +36,70 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //leftSpinner変数にplus_toneのstring-arrayを代入した変数"adapter"を代入する
         leftSpinner.setAdapter(adapter);
+        //せんたくされているアイテムのIndexを取得
+        int idx = leftSpinner.getSelectedItemPosition();
+
+        //〜〜〜ここでindexをsrotToneに処理させて、ソートを済ませたい〜〜〜
+        //ケース毎に引数を渡して、sortToneで処理させたい
+        switch(idx){
+            case 0:
+                //A,A#,B,C,C#,D,D#,E,F,F#,G,G#にソートする
+                sortToneUtil(0,28);
+                break;
+            case 1:
+                //A#,B,C,C#,D,D#,E,F,F#,G,G#,Aにソートする
+                sortToneUtil(2,30);
+                break;
+            case 2:
+                //B,C,C#,D,D#,E,F,F#,G,G#,A,A#にソートする
+                sortToneUtil(5,33);
+                break;
+            case 3:
+                //C,C#,D,D#,E,F,F#,G,G#,A,A#,Bにソートする
+                sortToneUtil(7,35);
+                break;
+            case 4:
+                //C#,D,D#,E,F,F#,G,G#,A,A#,B,Cにソートする
+                sortToneUtil(9,37);
+                break;
+            case 5:
+                //D,D#,E,F,F#,G,G#,A,A#,B,C,C#にソートする
+                sortToneUtil(12,40);
+                break;
+            case 6:
+                //D#,E,F,F#,G,G#,A,A#,B,C,C#,Dにソートする
+                sortToneUtil(14,42);
+                break;
+            case 7:
+                //E,F,F#,G,G#,A,A#,B,C,C#,D,D#にソートする
+                sortToneUtil(17,45);
+                break;
+            case 8:
+                //F,F#,G,G#,A,A#,B,C,C#,D,D#,Eにソートする
+                sortToneUtil(19,47);
+                break;
+            case 9:
+                //F#,G,G#,A,A#,B,C,C#,D,D#,E,Fにソートする
+                sortToneUtil(21,49);
+                break;
+            case 10:
+                //G,G#,A,A#,B,C,C#,D,D#,E,F,F#にソートする
+                sortToneUtil(24,52);
+                break;
+            case 11:
+                //G#,A,A#,B,C,C#,D,D#,E,F,F#,Gにソートする
+                sortToneUtil(26,54);
+                break;
+        }
+
+    }
+
+    private static final String SORT_STRING = "A,A#,B,C,C#,D,D#,E,F,F#,G,G#,A,A#,B,C,C#,D,D#,E,F,F#,G,";
+    //配列のソートを行う処理を描きたい
+    static void sortToneUtil(int i, int j) {
+        String result = SORT_STRING.substring(i,j);
+            //Spinnerで選択したIndex番号を
+        return ;
     }
 }
 
